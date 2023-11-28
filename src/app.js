d289c0e3edc55be900491e0d6cb0e4f878d0f34b
mjsn98 <mjsn98@gmail.com>
2023-11-28 10:12:39 -0300

    estructurando rutas

--- src/app.js
+++ src/app.js
@@ -1,20 +1,12 @@
-//Require de express
+
 const express=require('express');
-
-//Ejecucion de express
 const app=express(); 
-
-//Require path
 const path=require('path');
-
 const cartHtmlFile = path.join(__dirname, '/views/productCart.html');
-//Usando recursos estaticos
 app.use(express.static('public'));
 
-//Levantando el servidor Puerto 3080
 app.listen(3030,()=>console.log("Exito")); 
 
-//RUTAS
 app.get('/',(req,res)=>{ 
     res.sendFile(path.resolve(__dirname,'views/index.html')); 
 });
