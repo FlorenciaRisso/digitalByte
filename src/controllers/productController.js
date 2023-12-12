a81c4ec69eb37bb5d65024883871b88cb1f9003a
mjsn98 <mjsn98@gmail.com>
2023-12-12 20:55:26 -0300

    carga de json y edito mas buscados

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,5 @@
 const path=require('path');
+const productService = require('../data/productService');
 
 let productController = {
     index: (req, res)=>{
@@ -13,13 +14,14 @@ let productController = {
         res.render('products/productCart')
     },
     detalle:(req,res) => {
+        
         res.render('products/productDetail')
     },
     altaProducto:(req,res) => {
         res.render('products/altaProducto')
     },
     editProducto:(req,res) => {
-
+        let producto=productService.getOne(req);
         res.render('products/editProducto')
     },
     update:(req,res) => {
@@ -29,32 +31,8 @@ let productController = {
         res.redirect('/productos');
     },
     listarProductos:(req,res) => {
-        const productos = [
-            {
-              name: 'Ipad 8va Generacion',
-              description: 'El nuevo iPad se transforma en lo que quieras: un cuaderno digital, una oficina móvil, un estudio fotográfico, una consola de juegos o una sala de cine.',
-              image: '/img/apple/ipadGen8/ipad8va.png',
-              category: 'Tablets',
-              colors: ['Plata', 'Negro'],
-              price: 532.580
-            },
-            {
-              name: 'Ipad 9na Generacion',
-              description: 'Diseñado para que hagas todo lo que te gusta, por menos de lo que imaginas.',
-              image: '/img/apple/ipadGen9/ipad9na.png',
-              category: 'Tablets',
-              colors: ['Negro', 'Blanco'],
-              price: 649.999
-            },
-            {
-              name: 'Ipad 10ma Generacion',
-              description: 'Capturá todos tus momentos al instante gracias a sus cámaras, con resoluciones de 12 Mpx y 12 Mpx, podrás hacer videollamadas o sacarte fotos en cualquier momento y lugar, con una excelente calidad de imagen. Nitidez, brillo y colores vibrantes harán que tus experiencias se reflejen de manera óptima.',
-              image: '/img/apple/ipadGen10/ipad10ma.png',
-              category: 'Tablets',
-              colors: ['Azul', 'Gris', 'Negro'],
-              price: 765.809
-            }
-          ];
+        let productos = productService.products;
+        console.log(productos);
         res.render('products/listarProductos',{productos:productos})
     }
 }
--- src/data/productService.js
+++ src/data/productService.js
@@ -1,13 +1,18 @@
 const path = require('path');
-const fs=requise('fs');
-const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const fs=require('fs');
+const productsFilePath = path.join(__dirname, '../data/jsonProductos.json');
+const productsArray = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 let productService={
-    getOneProduct:function(req,res){
+    products:productsArray,
+    getAll:function(req,res){
+        return this.products;
+    },
+    getOne:function(req,res){
         let id=parseInt(req.params.id);
-        let producto=this.products
-
+        let producto=this.products.find((producto)=>{producto.id==id});
+        return producto;
     }
 
 }
+module.exports=productService;
