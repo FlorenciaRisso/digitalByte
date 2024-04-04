d177e0560e78cc4830ff96b7d53373c03dc3f5ba
mjsn98 <mjsn98@gmail.com>
2024-04-04 12:04:48 -0300

    add show hidden password in Registro

--- public/js/usuarios/changePassword.js
+++ public/js/usuarios/changePassword.js
@@ -149,19 +149,6 @@ window.addEventListener('load', function () {
         }
     });
 
-    let togglePasswords = document.querySelectorAll(".toggle-password");
-
-    togglePasswords.forEach(function (togglePassword) {
-        let passwordField = togglePassword.previousElementSibling; 
-
-        togglePassword.addEventListener("click", function () {
-            const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
-            passwordField.setAttribute("type", type);
-
-            this.classList.toggle("fa-eye-slash");
-            this.classList.toggle("fa-eye");
-        });
-    });
 
 
 });
