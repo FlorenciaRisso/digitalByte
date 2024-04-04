5b928b1a83083d2ef2eef431dadf67e4024d8262
mjsn98 <mjsn98@gmail.com>
2024-04-04 18:05:23 -0300

    middleware para categorias

---
+++ src/middlewares/productos/validarCategoria.js
@@ -0,0 +1,16 @@
+let productService = require('../../data/productService')
+const validarCategoria = async (req, res, next) => {
+
+    let categorias = await productService.getCategorias();
+
+    let idsCategorias=categorias.map(e=> e.id.toString());
+    let categoriaEnviada = req.query.cat;
+
+    if (!idsCategorias.includes(categoriaEnviada)) {
+        return res.status(404).render('error404');
+    }
+
+    next();
+};
+
+module.exports = validarCategoria;
--- src/routes/productos.js
+++ src/routes/productos.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const uploadFile=require('../data/multerMulti');
 const productController = require('../controllers/productController');
+const validarCategoria = require('../middlewares/productos/validarCategoria');
 const authMiddleware = require('../middlewares/usuarios/authMiddleware');
 const esAdmin = require('../middlewares/usuarios/esAdmin');
 const esCliente = require('../middlewares/usuarios/esCliente');
@@ -13,14 +14,16 @@ const editProductoValidation = require('../middlewares/productos/editProductoVal
 
 router.get('/', productController.index);
 router.post('/search', productController.search);
+
 router.get('/carrito',authMiddleware,esCliente, productController.carrito);//solo cliente
 router.put('/agregarCarrito/:id',authMiddleware,esCliente, productController.agregarAlCarrito);
+
 router.get('/lista',authMiddleware, esAdmin, productController.lista);//solo admin
 router.get('/filtro',authMiddleware, esVendedorOAdmin, productController.filtro);
 router.get('/listaMisProductos',authMiddleware, esVendedorOAdmin, productController.listaPorUsuario); //solo vendedor y admin
 //listar
 router.get('/listaproductos',authMiddleware,esVendedor, productController.listado);
-router.get('/categoria',productController.listaPorCat);
+router.get('/categoria',validarCategoria,productController.listaPorCat);
 //crear
 router.get('/create',authMiddleware,esVendedorOAdmin, productController.create);
 router.post('/create',authMiddleware,esVendedorOAdmin, uploadFile,createProductoValidation,productController.save);
